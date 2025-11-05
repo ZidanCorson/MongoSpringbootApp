eff4f9e9231d8a837d3f352ed0657cfcdf8f5bc3
ZidanCorson <zwang@scottlogic.com>
2025-11-05 09:31:42 +0000

    adds auto increment sequence

--- src/main/java/com/MongoSpring/MongoSpring/Model/Student.java
+++ src/main/java/com/MongoSpring/MongoSpring/Model/Student.java
@@ -2,8 +2,10 @@ package com.MongoSpring.MongoSpring.Model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -11,7 +13,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
+    @Transient
+    public static final String SEQUENCE_NAME = "student_sequence";
+
     @Id
+    @Generated
     private Integer rno;
 
     private String name;
